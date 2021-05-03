df23f2ff9e27bfde859925bee205c2a6bd07b580
Masalha-96 <Smasalha96@gmail.com>
2021-05-03 22:12:18 +0300

    update js & html files

--- class-07.js
+++ class-07.js
@@ -1,30 +1,55 @@
 "use strict";
+let time = [
+  "6am",
+  "7am",
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm",
+];
 
 function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-
-
-function PatStors(cityName, minCust, maxCust, avgCookie) 
+function fisrtCol ()
 {
-  this.time = [
-    "6am",
-    "7am",
-    "8am",
-    "9am",
-    "10am",
-    "11am",
-    "12pm",
-    "1pm",
-    "2pm",
-    "3pm",
-    "4pm",
-    "5pm",
-    "6pm",
-    "7pm",
-  ];
-  
+  let tr = document.createElement("tr");
+  table.appendChild(tr);
+  let th = document.createElement("th");
+  tr.appendChild(th);
+  th.textContent = "City Name / Hour";
+
+  for (let i = 0; i < time.length; i++) 
+  {
+    let th = document.createElement("th");
+    tr.appendChild(th);
+    th.textContent = `${time[i]}`;
+  }
+
+  th = document.createElement("th");
+  tr.appendChild(th);
+  th.textContent = "Daily Location Total";
+}
+
+/*
+<table> <tr> </tr> </table>
+*/
+let myDiv = document.getElementById("Sales");
+let table = document.createElement("table");
+myDiv.appendChild(table);
+
+function PatStors(cityName, minCust, maxCust, avgCookie) {
+
+
   this.numOfCust = 0;
 
   this.cityName = cityName;
@@ -37,60 +62,52 @@ function PatStors(cityName, minCust, maxCust, avgCookie)
     this.numOfCust = randomNum(minCust, maxCust);
   };
 
-  this.printAndTotal = function () 
-  {
-    let total = 0;
+  this.getRandomCust = function () {
+    randomNum(minCust, maxCust);
+  };
 
-    for (let i = 0; i < this.time.length; i++)
-    {
-      let listItems = document.createElement("li");
-      theParent.appendChild(listItems);
-      let temp = randomNum(this.minCust, this.maxCust);
-      listItems.textContent = `${this.time[i]}: ${temp} cookies`;
+
+
+  // addToTable function
+  this.addToTable = function () {
+    let total = 0;
+    let tr = document.createElement("tr");
+    table.appendChild(tr);
+    let th = document.createElement("th");
+    tr.appendChild(th);
+    th.textContent = `${this.cityName}`;
+
+    for (let i = 0; i < time.length; i++) {
+      let td = document.createElement("td");
+      tr.appendChild(td);
+      let temp = Math.floor(randomNum(this.minCust, this.maxCust) * avgCookie);
+      td.textContent = `${temp}`;
       total += temp;
     }
 
-    let listItems = document.createElement("li");
-    theParent.appendChild(listItems);
-    listItems.textContent = `Total: ${total} cookies`;
+    th = document.createElement("th");
+    tr.appendChild(th);
+    th.textContent = `${total}`;
   };
+
 }
 
 
-/* 1 */ 
-
-let theParent = document.getElementById("Sales");
 
 // -----------------------------------------------
-/* Seattle */
-let seattle = new PatStors("Seattle", 23, 65, 6.3); // creat obj
-let header2 = document.createElement("h3"); // creat tag
-theParent.appendChild(header2);  // set as a child 
-header2.textContent = seattle.cityName; // to give it text and show it in my html page
-
-let unOrederedList = document.createElement("ul"); // creat ul
-theParent.appendChild(unOrederedList);  // make ul as a child
-unOrederedList.textContent = seattle.printAndTotal(); // git ul the li valuse from for loop in the class
-
+let seattle = new PatStors("Seattle", 23, 65, 6.3); // creat objects from constructor
+let tokyo = new PatStors("Tokyo", 3, 24, 1.2); 
+let paris = new PatStors("Paris", 20, 38, 2.3);
+let lima = new PatStors("Lima", 2, 16, 4.6);
+let dubai = new PatStors("Dubai", 11, 38, 3.7);
 // -----------------------------------------------
-//Tokyo
-let tokyo = new PatStors ("Tokyo",3,24,1.2); // creat obj
-let header3 = document.createElement('h3');
-theParent.appendChild(header3);
-header3.textContent = tokyo.cityName;
 
-let ul2 = document.createElement('ul');
-theParent.appendChild(ul2);
-ul2.textContent = tokyo.printAndTotal(); 
-
-// -----------------------------------------------
-let dubai = new PatStors ("Dubai",11,38,3.7);
-
-
-// -----------------------------------------------
-// let paris = new PatStors ("Paris",20,38,2.3);
-
-// -----------------------------------------------
-// let lima = new PatStors ("Lima",2,16,4.6);
-
-// -----------------------------------------------
+// call the print method to show my elemnts in the html page----
+//seattle.colOne(); 
+fisrtCol();
+seattle.addToTable();                          
+tokyo.addToTable();
+paris.addToTable();
+dubai.addToTable();
+lima.addToTable();
+// --------------------------------------------------------------
